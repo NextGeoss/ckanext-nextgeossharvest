26cc2269aa55690b4932d392165586218a53f67d
Jovanka Gulicoska <jovanka.gulicoska@keitaro.com>
2018-05-22 16:44:03 +0200

    Fix error with dataset_per_page: The URL was always limited to get 1000 datasets

--- ckanext/nextgeossharvest/harvesters/esa.py
+++ ckanext/nextgeossharvest/harvesters/esa.py
@@ -143,7 +143,10 @@ class ESAHarvester(SentinelHarvester, OpenSearchHarvester, NextGEOSSHarvester):
         else:
             skip_raw = ''
 
-        harvest_url = '{base_url}/dhus/search?q=ingestiondate:{date_range}{skip_raw}&orderby=ingestiondate asc&start=0&rows=100'.format(base_url=base_url, date_range=date_range, skip_raw=skip_raw)  # noqa: E501
+        limit = self.source_config.get('datasets_per_job', 1000)
+
+        harvest_url = '{base_url}/dhus/search?q=ingestiondate:{date_range}{skip_raw}&orderby=ingestiondate asc&start=0&rows={limit}'.format(base_url=base_url, \
+                        date_range=date_range, skip_raw=skip_raw, limit=limit)  # noqa: E501
         log.debug('Harvest URL is {}'.format(harvest_url))
         username = config.get('ckanext.nextgeossharvest.nextgeoss_username')
         password = config.get('ckanext.nextgeossharvest.nextgeoss_password')
@@ -153,7 +156,6 @@ class ESAHarvester(SentinelHarvester, OpenSearchHarvester, NextGEOSSHarvester):
         # via cron jobs, we don't need to grab all the results from a date
         # range at once and the harvester will resume from the last gathered
         # date each time it runs.
-        limit = self.source_config.get('datasets_per_job', 1000)
         timeout = self.source_config.get('timeout', 4)
 
         if not hasattr(self, 'provider_logger'):
