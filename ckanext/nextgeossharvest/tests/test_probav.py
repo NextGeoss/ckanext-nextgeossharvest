6f7dddd1d3407b861803d4d936f8329fbdca0b79
davv-deimos <david.vicente@deimos.com.pt>
2018-05-25 18:38:23 +0100

    test for S type products and their metalinks

--- ckanext/nextgeossharvest/tests/test_probav.py
+++ ckanext/nextgeossharvest/tests/test_probav.py
@@ -22,50 +22,63 @@ class TestProvaVCollection(TestCase):
     def test_get_tags(self):
         collection = L2AProbaVCollection(ProductType.L2A, Resolution(333, Units.METERS))
         self.assertListEqual(collection.get_tags(), ['Proba-V', 'L2A', '333M'])
-    
+
+
+class TestSProbavHarvester(TestCase):
+
+    def setUp(self):
+        self.entry = read_first_entry('s1_100m.xml')
+        self.harvester = PROBAVHarvester()
+
+    def test_parse_metalink_url(self):
+        url = self.harvester._parse_metalink_url(self.entry)
+        self.assertEqual(url, "https://www.vito-eodata.be/PDF/dataaccess?service=DSEO&request=GetProduct&version=1.0.0&collectionID=1000125&productID=267473044&ProductURI=urn:ogc:def:EOP:VITO:PROBAV_S1-TOA_100M_V001:PROBAV_S1-TOA_20180101_100M:V101&")
+
+
+def read_first_entry(filename):
+    filepath = path.join(path.dirname(__file__), filename)
+    with open(filepath, 'r') as open_search_file:
+        open_search_resp = BeautifulSoup(open_search_file.read(), 'lxml-xml')
+        return open_search_resp.entry
+
+
 class TestProbavHarvester(TestCase):
 
+    def setUp(self):
+        self.entry = read_first_entry('l2a_entry.xml')
+        self.harvester = PROBAVHarvester()
+
     def test_parse_identifier_element(self):
-        entry = self.read_first_entry('l2a_entry.xml')
-        harvester = PROBAVHarvester()
-        identifier = harvester._parse_identifier_element(entry)
+        identifier = self.harvester._parse_identifier_element(self.entry)
         self.assertEqual(identifier, 'urn:ogc:def:EOP:VITO:PROBAV_L2A_333M_V001:PROBAV_CENTER_L2A_20180101_005544_333M:V101')
 
     def test_parse_interval(self):
-        entry = self.read_first_entry('l2a_entry.xml')
-        harvester = PROBAVHarvester()
-        start, end = harvester._parse_interval(entry)
+        start, end = self.harvester._parse_interval(self.entry)
         self.assertEqual(start, "2018-01-01T00:55:44Z")
         self.assertEqual(end, "2018-01-01T01:02:21Z")
-    
+
     def test_parse_identifier(self):
         identifier = 'urn:ogc:def:EOP:VITO:PROBAV_L2A_333M_V001:PROBAV_CENTER_L2A_20180101_005544_333M:V101'
-        harvester = PROBAVHarvester()
-        identifier = harvester._parse_identifier(identifier)
+        identifier = self.harvester._parse_identifier(identifier)
         self.assertEqual(identifier, 'PROBAV_CENTER_L2A_20180101_005544_333M_V101')
 
     def test_parse_name(self):
         identifier = 'urn:ogc:def:EOP:VITO:PROBAV_L2A_333M_V001:PROBAV_CENTER_L2A_20180101_005544_333M:V101'
-        harvester = PROBAVHarvester()
-        name = harvester._parse_name(identifier)
+        name = self.harvester._parse_name(identifier)
         self.assertEqual(name, 'probav_center_l2a_20180101_005544_333m_v101')
 
     def test_parse_filename(self):
         identifier = 'urn:ogc:def:EOP:VITO:PROBAV_L2A_333M_V001:PROBAV_CENTER_L2A_20180101_005544_333M:V101'
-        harvester = PROBAVHarvester()
-        filename = harvester._parse_filename(identifier)
+        filename = self.harvester._parse_filename(identifier)
         self.assertEqual(filename, 'PROBAV_CENTER_L2A_20180101_005544_333M_V101.HDF5')
-    
+
     def test_prase_bounding_box(self):
-        entry = self.read_first_entry('l2a_entry.xml')
-        harvester = PROBAVHarvester()
-        bbox = harvester._parse_bbox(entry)
+        bbox = self.harvester._parse_bbox(self.entry)
         self.assertEqual(bbox, [40.341, 145.476, 65.071, 165.962992])
 
     def test_prase_bounding_box_to_geojson(self):
         bbox = [40.341, 145.476, 65.071, 165.962992]
-        harvester = PROBAVHarvester()
-        geojson = harvester._bbox_to_geojson(bbox)
+        geojson = self.harvester._bbox_to_geojson(bbox)
         self.assertDictEqual(geojson, {
             'type': 'Polygon',
             'crs': {
@@ -81,12 +94,11 @@ class TestProbavHarvester(TestCase):
                              [145.476, 40.341],
                              [145.476, 65.071]]]
             })
-        
+
     def test_bbox_to_polygon(self):
         bbox = [40.341, 145.476, 65.071, 165.962992]
-        harvester = PROBAVHarvester()
-        polygon = harvester._bbox_to_polygon(bbox)
-        self.assertEqual(polygon, 
+        polygon = self.harvester._bbox_to_polygon(bbox)
+        self.assertEqual(polygon,
                          [[145.476, 65.071],
                           [165.962992, 65.071],
                           [165.962992, 40.341],
@@ -95,8 +107,7 @@ class TestProbavHarvester(TestCase):
 
     def test_parse_L2A_collection_from_identifier(self):
         identifier = 'urn:ogc:def:EOP:VITO:PROBAV_L2A_100M_V001:PROBAV_CENTER_L2A_20180101_005544_100M:V101'
-        harvester = PROBAVHarvester()
-        collection  = harvester._parse_collection_from_identifier(identifier)
+        collection  = self.harvester._parse_collection_from_identifier(identifier)
         self.assertIsInstance(collection, L2AProbaVCollection)
         self.assertEqual(collection.product_type, ProductType.L2A)
         self.assertEqual(collection.resolution.units, Units.METERS)
@@ -104,8 +115,7 @@ class TestProbavHarvester(TestCase):
  
     def test_parse_S1_TOC_NDVI_collection_from_identifier(self):
         identifier = 'urn:ogc:def:EOP:VITO:PROBAV_S1-TOC-NDVI_100M_V001:PROBAV_S1-TOC-NDVI_20180101_100M:V101'
-        harvester = PROBAVHarvester()
-        collection  = harvester._parse_collection_from_identifier(identifier)
+        collection  = self.harvester._parse_collection_from_identifier(identifier)
         self.assertIsInstance(collection, SProbaVCollection)
         self.assertEqual(collection.frequency, 1)
         self.assertEqual(collection.product_type, ProductType.TOC)
@@ -113,9 +123,7 @@ class TestProbavHarvester(TestCase):
         self.assertEqual(collection.resolution.value, 100)
 
     def test_parse_L2A_content(self):
-        entry = str(self.read_first_entry('l2a_entry.xml'))
-        harvester = PROBAVHarvester()
-        parsed_content = harvester._parse_content(entry)
+        parsed_content = self.harvester._parse_content(str(self.entry))
         self.assertIsNotNone(parsed_content.get('uuid'))
         del parsed_content['uuid']
         expected_parsed_content = {
@@ -144,35 +152,21 @@ class TestProbavHarvester(TestCase):
         self.assertDictEqual(parsed_content, expected_parsed_content)
         self.assertDictEqual(spatial, expected_spatial)
 
-    def read_first_entry(self, filename):
-        filepath = path.join(path.dirname(__file__), filename)
-        with open(filepath, 'r') as open_search_file:
-            open_search_resp = BeautifulSoup(open_search_file.read(), 'lxml-xml')
-            return open_search_resp.entry
-
     def test_get_metadata_url(self):
-        entry = self.read_first_entry('l2a_entry.xml')
-        harvester = PROBAVHarvester()
-        metadata_url = harvester._get_metadata_url(entry)
+        metadata_url = self.harvester._get_metadata_url(self.entry)
         self.assertEqual(metadata_url, "https://www.vito-eodata.be/PDF/dataaccessMdXML?mdmode=hma&collectionID=1000126&productID=267446487&fileName=PV_CENTER_L2A-20180101005544_333M_V101.xml")
 
     def test_get_product_url(self):
-        entry = self.read_first_entry('l2a_entry.xml')
-        harvester = PROBAVHarvester()
-        product_url = harvester._get_product_url(entry)
+        product_url = self.harvester._get_product_url(self.entry)
         self.assertEqual(product_url, "https://www.vito-eodata.be/PDF/dataaccess?service=DSEO&request=GetProduct&version=1.0.0&collectionID=1000126&productID=267446487&ProductURI=urn:ogc:def:EOP:VITO:PROBAV_L2A_333M_V001:PROBAV_CENTER_L2A_20180101_005544_333M:V101&")
 
     def test_get_thumbnail_url(self):
-        entry = self.read_first_entry('l2a_entry.xml')
-        harvester = PROBAVHarvester()
-        thumbnail_url = harvester._get_thumbnail_url(entry)
+        thumbnail_url = self.harvester._get_thumbnail_url(self.entry)
         self.assertEqual(thumbnail_url, "https://www.vito-eodata.be/cgi-bin/probav?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&SRS=EPSG:4326&FORMAT=image/png&LAYERS=PROBAV_L2A_333M_Red band&TIME=2018-01-01T00:55:44Z&BBOX=40.341,145.476,65.071,165.962992&HEIGHT=200&WIDTH=166")
 
     def test_get_resources(self):
-        entry = str(self.read_first_entry('l2a_entry.xml'))
-        harvester = PROBAVHarvester()
-        content = harvester._parse_content(entry)
-        resources = harvester._get_resources(content)
+        content = self.harvester._parse_content(str(self.entry))
+        resources = self.harvester._get_resources(content)
         self.assertListEqual(resources, [
             {
                 'name': 'Metadata Download',
