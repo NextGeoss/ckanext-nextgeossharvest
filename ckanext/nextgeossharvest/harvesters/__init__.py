080136dfa678fe0ee5f2bf3bc01eb3981b5d0441
davv-deimos <david.vicente@deimos.com.pt>
2018-07-04 11:58:39 +0100

    removed proba v import, not relevant for this branch

--- ckanext/nextgeossharvest/harvesters/__init__.py
+++ ckanext/nextgeossharvest/harvesters/__init__.py
@@ -1,4 +1,3 @@
 from ckanext.nextgeossharvest.harvesters.cmems import CMEMSHarvester  # noqa: F401, E501
 from ckanext.nextgeossharvest.harvesters.esa import ESAHarvester  # noqa: F401, E501
-from ckanext.nextgeossharvest.harvesters.probav import PROBAVHarvester  # noqa: F401, E501
 from ckanext.nextgeossharvest.harvesters.itag import ITagEnricher  # noqa: F401, E501
