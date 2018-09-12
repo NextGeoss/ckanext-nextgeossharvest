cc66a3d13e1d3e48e8f43a72a54ddca42e7a6213
davv-deimos <david.vicente@deimos.com.pt>
2018-09-12 15:47:17 +0100

    Aditional identation fixes according PEP8

--- ckanext/nextgeossharvest/harvesters/cmems.py
+++ ckanext/nextgeossharvest/harvesters/cmems.py
@@ -225,7 +225,7 @@ class FtpSource(object):
             ftp.cwd('/{}/{}'.format(self._get_ftp_path(), directory))
             ftp_urls |= set(self._ftp_url(directory, fname)
                             for fname in ftp.nlst(
-                            ) if self.fname_pattern.match(
+                                ) if self.fname_pattern.match(
                                 fname) and self._to_harvest(
                                     fname, start_date, end_date)
                             )
