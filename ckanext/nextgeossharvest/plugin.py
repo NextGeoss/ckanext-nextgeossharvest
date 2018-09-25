9b79e3594be3bc0d0e3239c1285d7d697f03efff
Jovanka Gulicoska <jovanka.gulicoska@keitaro.com>
2018-09-25 12:59:56 +0200

    Remove templates and helpers for package from the extension

--- ckanext/nextgeossharvest/plugin.py
+++ ckanext/nextgeossharvest/plugin.py
@@ -5,16 +5,3 @@ from ckanext.nextgeossharvest import helpers
 
 class NextgeossharvestPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
-    plugins.implements(plugins.ITemplateHelpers, inherit=True)
-
-    # IConfigurer
-
-    def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-
-    # ITemplateHelpers
-
-    def get_helpers(self):
-        return {
-            'harvest_sorted_extras': helpers.harvest_sorted_extras,
-        }
