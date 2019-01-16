63430d0db660218baf3ec287b2b0b5e677016175
davv-deimos <joao.andrade@deimos.com.pt>
2019-01-16 13:14:35 +0000

    New harvest added for the GDACS Data Source

--- ckanext/nextgeossharvest/lib/nextgeoss_base.py
+++ ckanext/nextgeossharvest/lib/nextgeoss_base.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import ast
 import json
 import logging
 import os
@@ -44,6 +45,35 @@ class NextGEOSSHarvester(HarvesterBase):
             if extra.key == key:
                 return extra.value
         return default
+
+    def _get_package_extra(self, package_dict, flagged_extra, default=None):
+        """
+        Helper method for retrieving the value from a package's extras list.
+        """
+        extras = self.convert_string_extras(package_dict['extras'])
+
+        if "dataset_extra" in extras:
+            extras = ast.literal_eval(extras["dataset_extra"])
+
+        if type(extras) == list:
+            for extra in extras:
+                if extra["key"] == flagged_extra:
+                    return extra["value"]
+        elif type(extras) == dict:
+            value = extras.get(flagged_extra)
+            if value:
+                return value
+
+        return default
+
+    def convert_string_extras(self, extras_list):
+        """Convert extras stored as a string back into a normal extras list."""
+        try:
+            extras = ast.literal_eval(extras_list[0]["value"])
+            assert type(extras) == list
+            return extras
+        except (Exception, AssertionError):
+            return extras_list
 
     def _set_source_config(self, config_str):
         '''
@@ -280,6 +310,26 @@ class NextGEOSSHarvester(HarvesterBase):
             handler = logging.StreamHandler()
             handler.setFormatter(logging.Formatter('%(levelname)s | %(message)s'))  # noqa: E501
             logger = logging.getLogger('provider_logger')
+            logger.setLevel(logging.INFO)
+            logger.addHandler(handler)
+
+        return logger
+
+    def make_harvester_logger(self, filename='dataconnectors_info.log'):
+        """Create a logger just for datasets harvested."""
+        log_dir = config.get('ckanext.nextgeossharvest.provider_log_dir')
+        if log_dir:
+            if not os.path.exists(log_dir):
+                os.makedirs(log_dir)
+            handler = logging.FileHandler('{}/{}'.format(log_dir, filename))
+            handler.setFormatter(logging.Formatter('%(levelname)s | %(message)s'))  # noqa: E501
+            logger = logging.getLogger('harvester_logger')
+            logger.setLevel(logging.INFO)
+            logger.addHandler(handler)
+        else:
+            handler = logging.StreamHandler()
+            handler.setFormatter(logging.Formatter('%(levelname)s | %(message)s'))  # noqa: E501
+            logger = logging.getLogger('harvester_logger')
             logger.setLevel(logging.INFO)
             logger.addHandler(handler)
 
