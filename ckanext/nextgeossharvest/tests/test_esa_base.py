10e5503354b0c7b1fd4a1959ee081d437b39f9d4
João Andrade <joao.andrade@deimos.com.pt>
2019-02-21 18:25:35 +0000

    Update test_esa_base.py

--- ckanext/nextgeossharvest/tests/test_esa_base.py
+++ ckanext/nextgeossharvest/tests/test_esa_base.py
@@ -38,7 +38,7 @@ class TestNormalizeNames(object):
             'identifier': 'S1B_EW_GRDH_1SDH_20180131T104713_20180131T104813_009414_010EA4_BD6D',  # noqa: E501
             'size': '1.0 GB',
             'PlatformIdentifier': '2016-025A',
-            'RelativeOrbitNumber': '144'
+            'RelativeOrbitNumber': '113'
         }
 
         assert test_item == expected_item
