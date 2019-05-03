096a463a136d4e60b3cf365e66253c1585e2f25e
João Andrade <joao.andrade@deimos.com.pt>
2019-05-03 17:14:43 +0100

    modis entry point added
    modis entry point added

--- setup.py
+++ setup.py
@@ -87,6 +87,7 @@ setup(
         probav=ckanext.nextgeossharvest.harvesters:PROBAVHarvester
         glass_lai=ckanext.nextgeossharvest.harvesters:GLASS_LAI_Harvester
         plan4all=ckanext.nextgeossharvest.harvesters:Plan4AllHarvester
+	modis=ckanext.nextgeossharvest.harvesters:MODISHarvester
         itag=ckanext.nextgeossharvest.harvesters:ITagEnricher
 	      ebvs=ckanext.nextgeossharvest.harvesters:EBVSHarvester
         [babel.extractors]
