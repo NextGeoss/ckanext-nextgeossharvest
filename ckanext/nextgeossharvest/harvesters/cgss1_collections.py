feb4a869789f4dd9c3c7f44da2b8c8f35a053b4d
davidcordeiro-deimos <david.cordeiro@deimos.com.pt>
2019-09-04 17:08:01 +0100

    First version of VITO CGS S1 Harvester

---
+++ ckanext/nextgeossharvest/harvesters/cgss1_collections.py
@@ -0,0 +1,19 @@
+COLLECTION_DESCRIPTIONS = {
+    "CGS S1 SLC L1":
+    ("S1 datasets processed on VITO premises. Level-1 Single Look Complex "
+     "(SLC) products consist of focused SAR data, geo-referenced using orbit "
+     "and attitude data from the satellite, and provided in slant-range "
+     "geometry. Slant range is the natural radar range observation coordinate, "
+     "defined as the line-of-sight from the radar to each reflecting object. "
+     "The products are in zero-Doppler orientation where each row of pixels "
+     "represents points along a line perpendicular to the sub-satellite track."),
+     "CGS S1 GRD SIGMA0 L1":
+    ("Level-1 Ground Range Detected (GRD) products consist of focused SAR data "
+     "that has been detected, multi-looked and projected to ground range using "
+     "the Earth ellipsoid model WGS84. The ellipsoid projection of the GRD "
+     "products is corrected using the terrain height specified in the product "
+     "general annotation. The terrain height used varies in azimuth but is "
+     "constant in range (but can be different for each IW/EW sub-swath)."),
+    "CGS S1 GRD L1":
+    ("TBD"),
+}
