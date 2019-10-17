e967cb8e5671835a41a5e23f7a79c0d95c70037e
David Cordeiro <david.cordeiro@deimos.com.pt>
2019-10-17 15:45:31 +0100

    Add Landsat-8 harvester

---
+++ ckanext/nextgeossharvest/harvesters/landsat8_collections.py
@@ -0,0 +1,26 @@
+COLLECTION_DESCRIPTIONS = {
+
+        "Landsat-8 T1":  ("Contains the highest quality Level-1 Precision"
+                          " Terrain (L1TP) data considered suitable for "
+                          "time-series analysis. The georegistration is "
+                          "consistent and within prescribed tolerances "
+                          "[<12m root mean square error (RMSE)]."),
+
+        "Landsat-8 T2":  ("Contains L1TP scenes not meeting Tier 1 "
+                          "criteria and all Systematic Terrain (L1GT) "
+                          "and Systematic (L1GS) scenes. Users interested "
+                          "in Tier 2 scenes can evaluate the L1TP RMSE and "
+                          "other properties to determine suitability for "
+                          "use in their applications and studies."),
+
+        "Landsat-8 RT":  ("Contains newly acquired Landsat 8 scenes, "
+                          "which require a period of evaluation and "
+                          "calibration adjustment after acquisition but "
+                          "are processed immediately based on preliminary "
+                          "calibration coefficients, assigned to the "
+                          "temporary RT Tier, and made available for "
+                          "download. When definitive calibration information "
+                          "becomes available, these scenes are reprocessed, "
+                          "assigned to the appropriate Tier 1 or Tier 2 "
+                          "category, and removed from the RT Tier "
+                          "(Temporary designation).")}
