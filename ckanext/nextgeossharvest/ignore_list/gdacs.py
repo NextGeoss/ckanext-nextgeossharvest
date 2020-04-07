d7209bcf910256aa29b1d843f5e1b6aec6a66981
davidcordeiro-deimos <46027889+davidcordeiro-deimos@users.noreply.github.com>
2020-04-07 11:46:32 +0100

    Remove fields from solr indexes (#202)
    * - Add feature to remove fields from index (sentinel)
    
    * Fix spelling errors and matching keys
    
    * Fix update_extras to replace
    
    * Debug test - no reindex
    
    * Debug test 2: with field removal
    
    * Debug test 1 - field removal commented
    
    * debug test 2: removing fields from index
    
    * Added Theme
    
    * Remove extras_* from solr
    Add ignore_list as configurable file
    
    * Fix import error
    Filter indexes for dataset only
    
    * Add missing sentinel collection
    
    * Undo debug commit and revert changes in nextgeoss_base
    
    * Undo debug commit
    
    * Add gome2 fields to solr ignore list
    
    * Refactor collection_ids to respective file
    Add probav fields to solr ignore list
    
    * Add modis fields to solr ignore list #136
    
    * Add foodsecurity fields to solr ignore list #140
    
    * Add deimosimg fields to solr ignore list #144
    
    * Fix spelling error
    
    * Fix collection_id key
    
    * Add plan4all fields to solr ignore list #151
    
    * Add cmems fields to solr ignore list #154
    
    * Add ebvs fields to solr ignore list #156
    
    * Add vitocgs fields to solr ignore list #158
    
    * Add simocean fields to solr ignore list #171
    
    * Add ebas fields to solr ignore list #184
    
    * Add "Matrix" to ebas solr ignore list
    
    * Add epos fields to solr ignore list #188
    
    * Add gdacs fields to solr ignore list #142
    
    * Change log messages
    Add missing field to EBAS ignore list

---
+++ ckanext/nextgeossharvest/ignore_list/gdacs.py
@@ -0,0 +1,11 @@
+GDACS_IGNORE_LIST = [
+        "StartDate",
+        "Startdate",
+        "filename",
+        "downloadLink"
+]
+
+GDACS_IGNORE_DICT = {
+    "AVERAGE_FLOOD_SIGNAL": GDACS_IGNORE_LIST,
+    "AVERAGE_FLOOD_MAGNITUDE": GDACS_IGNORE_LIST
+}
