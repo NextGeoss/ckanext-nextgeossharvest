b7aaa9ef4f15e800250e60788ff7eb0cdb54bb3f
João Andrade <joao.andrade@deimos.com.pt>
2020-04-28 16:48:44 +0100

    Update landsat8.py

--- ckanext/nextgeossharvest/harvesters/landsat8.py
+++ ckanext/nextgeossharvest/harvesters/landsat8.py
@@ -214,11 +214,12 @@ class Landsat8Harvester(NextGEOSSHarvester):
 
             scene_ids = [os.path.basename(key.strip('/')) for key in results]
 
+        if 'update_all' in self.source_config
+            update_all = self.source_config['update_all']
+        else:
+            update_all = False
+
         for scene in scene_ids:
-            if 'update_all' in self.source_config
-                update_all = self.source_config['update_all']
-            else:
-                update_all = False
             _id = self._gather_entry(scene, int(path), int(row), update_all)
             if _id:
                 ids.append(_id)
